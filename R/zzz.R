8d67ce6d50ec9bc423f0fe63969e84b50490385d
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-09-13 18:09:55 -0300

    Rompiendo el plot

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,5 @@ contribuyentes <- tibble::tribble(
   ~Nombre, ~Pais,
   # Cada contribuyente agrega una fila con su nombre-apellido y su bandera.
   # Ordenados alfabeticamente. Por ejemplo,
-  "Juan Cruz Rodriguez", emojifont::emoji(tolower("Argentina")),
+  "Juan Cruz Rodriguez", emojifont::emoji(tolower("LatinoAmerica")),
 )
