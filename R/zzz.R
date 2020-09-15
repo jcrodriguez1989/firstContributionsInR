f750373187a5f2b7d4dd82acd26d30ec7d508696
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-09-14 22:13:31 -0300

    Agregando a Juana Perez como contribuyente

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,5 @@ contribuyentes <- tibble::tribble(
   ~Nombre, ~Pais,
   # Cada contribuyente agrega una fila con su nombre-apellido y su bandera.
   # Ordenados alfabeticamente. Por ejemplo,
-  "Juan Cruz Rodriguez", emojifont::emoji("Argentina"),
+  # "Juan Cruz Rodriguez", emojifont::emoji("Argentina"),
 )
