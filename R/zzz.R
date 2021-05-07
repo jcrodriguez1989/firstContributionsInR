2d32144713b260a92e6729195c227e93faeddcb9
martinnnuez <64661515+martinnnuez@users.noreply.github.com>
2021-05-07 18:06:03 -0300

    Rodriguez nunez martin (#10)
    * “Agregando a Rodriguez Nuñez Martin como contribuyente”
    
    * Puse tilde a martin

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,7 @@ contribuyentes <- tibble::tribble(
   # Cada contribuyente agrega una fila con su nombre-apellido y su bandera.
   # Ordenados alfabeticamente. Por ejemplo,
   "Juan Cruz Rodriguez", emojifont::emoji(tolower("Argentina")),
+  "Martín Rodriguez Nuñez", emojifont::emoji(tolower("Argentina")),
   "Natalia Morandeira", emojifont::emoji(tolower("Argentina")),
   "Julio Augusto Spairani", emojifont::emoji(tolower("Argentina")),
 )
